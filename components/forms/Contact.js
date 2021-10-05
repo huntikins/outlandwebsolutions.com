1edfee25043a77d21f59cd9c86353509220c1412
Hunter Trammell <hunterttrammell@gmail.com>
2021-10-04 23:06:59 -0500

    updated hero section

--- components/forms/Contact.js
+++ components/forms/Contact.js
@@ -1,78 +1,79 @@
-import styles from './Contact.module.css'
+import styles from './Contact.module.scss'
 
 export default function ContactForm() {
 	return (
 		<form className={styles.form}>
-			<h2>CTA goes here</h2>
 			<div className={styles.formGroup}>
-				<label id="email-label" for="email-input" data-required="true">
-					Email
+				<h1>Let's find your web solution together</h1>
+				<label id="email-label" htmlFor="email-input" data-required="true">
+					Email<span>*</span>
 				</label>
 				<input
 					id="email-input"
 					aria-labelledby="email-label"
 					type="email"
 					name="email"
-					autocomplete="email"
+					autoComplete="email"
 					required
 					placeholder=""
-					inputmode="email"
+					inputMode="email"
 					aria-invalid="true"
 					aria-required="true"
 					data-field-type="email"
 				/>
 			</div>
 			<div className={styles.formGroup}>
-				<label id="firstname-label" for="firstname-input" data-required="true">
-					First Name
+				<label
+					id="firstname-label"
+					htmlFor="firstname-input"
+					data-required="true"
+				>
+					First Name<span>*</span>
 				</label>
 				<input
 					id="firstname-input"
 					aria-labelledby="firstname-label"
 					type="text"
 					name="firstname"
-					autocomplete="firstname"
+					autoComplete="firstname"
 					required
 					placeholder=""
-					inputmode="firstname"
+					inputMode="firstname"
 					aria-invalid="true"
 					aria-required="true"
 					data-field-type="firstname"
 				/>
 			</div>
 			<div className={styles.formGroup}>
-				<label id="lastname-label" for="lastname-input" data-required="true">
-					Last Name
+				<label
+					id="lastname-label"
+					htmlFor="lastname-input"
+					data-required="true"
+				>
+					Last Name<span>*</span>
 				</label>
 				<input
 					id="lastname-input"
 					aria-labelledby="lastname-label"
 					type="text"
 					name="lastname"
-					autocomplete="lastname"
+					autoComplete="lastname"
 					required
 					placeholder=""
-					inputmode="lastname"
+					inputMode="lastname"
 					aria-invalid="true"
 					aria-required="true"
 					data-field-type="lastname"
 				/>
 			</div>
 			<div className={styles.formGroup}>
-				<label id="message-label" for="message-input" data-required="true">
+				<label id="message-label" htmlFor="message-input" data-required="true">
 					Message
 				</label>
 				<textarea
 					id="message-input"
 					aria-labelledby="message-label"
 					name="message"
-					autocomplete="message"
-					required
-					placeholder=""
-					inputmode="message"
-					aria-invalid="true"
-					aria-required="true"
-					data-field-type="message"
 				/>
 			</div>
 			<div className={styles.formGroup}>
@@ -81,7 +82,11 @@ export default function ContactForm() {
 					to store and process the personal information submitted above to
 					provide you the content requested.
 				</p>
-				<input type="submit" value="Submit" className={styles.submit} />
+				<input
+					type="submit"
+					value="Submit"
+					className="btn btn-primary btn-block"
+				/>
 			</div>
 		</form>
 	)
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import { Fragment } from 'react'
 import Head from 'next/head'
 
