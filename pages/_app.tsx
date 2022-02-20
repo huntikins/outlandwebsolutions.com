703f29da88e259d8d563fe063dae1e86538121c3
Hunter Trammell <huntertrammell@Ceris-Air.lan>
2022-02-20 09:01:10 -0600

    added gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,29 @@ import type { AppProps } from 'next/app'
 import Nav from '../components/nav/Nav'
 import Head from 'next/head'
 import Footer from '../components/nav/Footer'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/*<!-- End Google Tag Manager -->*/}
+      <Script
+        id="google-tag-manager"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TT6N3K2');`,
+        }}
+      />
+      {/*<!-- End Google Tag Manager -->*/}
       <Head>
         <title>Outland Web Solutions</title>
-        <meta name="description" content="Full Service Web Development, Hosting, Maintenance & SEO" />
+        <meta
+          name="description"
+          content="Full Service Web Development, Hosting, Maintenance & SEO"
+        />
       </Head>
       <Nav />
       <Component {...pageProps} />
